6fdd2edf4fab5aeca70755dffdc1a15259a39595
xsanm <kkurowski.kk@gmail.com>
2022-05-07 16:50:01 +0200

    Netlify deploy config

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Wrapper from "../components/Wrapper";
 
 const Homepage: React.FC = () => {
-  return <Wrapper>Homepage - jakieś buttony i logo</Wrapper>;
+  return <Wrapper>Homepage - jakieś buttony i logo <h1>Deployed</h1></Wrapper>;
 };
 
 export default Homepage;
