f416e761d014db0a37cc4f925ec000ebcaf0fbb4
Arkadiusz Wolk <awolk@student.agh.edu.pl>
2022-05-14 15:00:39 +0200

    refactor

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,29 +1,8 @@
 import React from "react";
 import Wrapper from "../components/Wrapper";
-import Question from "../components/Question";
 
 const Homepage: React.FC = () => {
-  const onResult = (correctAnswer: boolean) => {
-    console.log(`answer: ${correctAnswer}`)
-  }
-
-  const question = {
-    question: "Pytanie?",
-    answers: [
-      {text: 'A', isCorrect: false},
-      {text: 'B', isCorrect: false},
-      {text: 'C', isCorrect: false},
-      {text: 'D', isCorrect: true},
-    ]
-  }
-
-  return (
-      <Wrapper>
-        Homepage - jakieś buttony i logo
-        <h1>Deployed</h1>
-        <Question question={question} hackName={"NAZWA HACKA"} onResult={onResult}/>
-      </Wrapper>
-  );
+  return <Wrapper>Homepage - jakieś buttony i logo<h1>Deployed</h1></Wrapper>;
 };
 
 export default Homepage;
