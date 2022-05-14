5642b40694dc7003f294080c4d63e63e20f2ea7b
xsanm <kkurowski.kk@gmail.com>
2022-05-14 15:15:37 +0200

    Implement Game logic

--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -1,15 +1,20 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import Board from "./Board";
-import { MockGameState } from "../const/const";
 import Keyboard from "./Keyboard";
 import Hackbar from "./Hackbar";
+import { ColorType, GameElementType, GameState } from "../const/types";
+import { GameContendContext } from "../App";
 
 const Game: React.FC = () => {
-  // const content = useContext(GameContendContext);
-  // console.log(content);
+  const content = useContext(GameContendContext);
 
   const [innerInput, setInnerInput] = useState<string>("");
   const [columns, setColumns] = useState(10);
+  const [gameState, setGameState] = useState<GameState>({
+    rowsNumber: 0,
+    rows: [],
+  });
+  const [enter, setEnter] = useState(false);
 
   const handleBackspace = useCallback(() => {
     setInnerInput((prevState) => prevState.slice(0, -1));
@@ -21,21 +26,54 @@ const Game: React.FC = () => {
         prevState.length < columns ? prevState + val : prevState
       );
     },
-    [setInnerInput, columns]
+    [columns]
   );
 
-  const handleEnter = useCallback(() => {
-    console.log("Enter pressed");
-  }, []);
+  useEffect(() => {
+    if (!enter) {
+      return;
+    }
+    if (!content.dictionary?.includes(innerInput.toLowerCase())) {
+      setEnter(false);
+      return;
+    }
+    const lettersColored: GameElementType[] = innerInput
+      .split("")
+      .map((letter, id) => {
+        let color: ColorType = "GRAY";
+        if (content.wordOfDay?.toUpperCase().includes(letter)) {
+          color = "YELLOW";
+        }
+        if (content.wordOfDay?.[id]?.toUpperCase() === letter) {
+          color = "GREEN";
+        }
+        return {
+          color,
+          letter,
+        };
+      });
+    setGameState((prevState) => ({
+      rowsNumber: prevState.rowsNumber + 1,
+      rows: [
+        ...prevState.rows,
+        { elements: lettersColored, length: lettersColored.length },
+      ],
+    }));
+    setEnter(false);
+    setInnerInput("");
+  }, [innerInput, gameState, enter, content]);
 
-  console.log(innerInput);
+  const handleEnter = () => {
+    setEnter(true);
+    console.log("Enter pressed");
+  };
 
   return (
     <div className="w-full m-auto">
       <Board
         cols={columns}
         rows={8}
-        gameState={MockGameState}
+        gameState={gameState}
         innerState={innerInput}
       />
       <Hackbar
@@ -47,7 +85,11 @@ const Game: React.FC = () => {
           () => console.log("ADD COIL"),
         ]}
       />
-      <Keyboard handleBackspace={handleBackspace} handleLetter={handleLetter} handleEnter={handleEnter} />
+      <Keyboard
+        handleBackspace={handleBackspace}
+        handleLetter={handleLetter}
+        handleEnter={handleEnter}
+      />
     </div>
   );
 };
