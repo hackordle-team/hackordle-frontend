806557f2f91a2b9911558b0e12dc9d5d1cddfc4a
Peter <pstecyk@student.agh.edu.pl>
2022-05-15 15:17:19 +0200

    nav font fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,9 @@ const Navbar: React.FC = () => {
     <nav className="flex justify-between items-center bg-black text-white px-8 py-5 w-full">
       <Link to="/">Hackordle</Link>
       <div className="flex gap-x-10 mr-6">
-        <Link to="/">Singleplayer</Link>
-        <Link to="/multi">Mutliplayer</Link>
-        <Link to="/help">Help</Link>
+        <Link className="text-white font-bold" to="/">Singleplayer</Link>
+        <Link className="text-white font-bold" to="/multi">Mutliplayer</Link>
+        <Link className="text-white font-bold" to="/help">Help</Link>
       </div>
     </nav>
   );
